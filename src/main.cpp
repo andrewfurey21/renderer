76cef6f70f06fca9014a45f2dffc21f70a931e75
andrewfurey21 <andrewfurey2003@gmail.com>
2025-07-08 11:59:18 +0100

    color background

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
 
-#include <glad/glad.h>
+#include "../include/glad/glad.h"
 #include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
 int main(void) {
-  std::cout << "hello world\n";
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  size_t windowWidth = 800;
+  size_t windowHeight = 600;
+
+  GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Renderer", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create window.\n";
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD.\n";
+    glfwTerminate();
+    return -1;
+  }
+
+  glViewport(0, 0, windowWidth, windowHeight);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    process_input(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
