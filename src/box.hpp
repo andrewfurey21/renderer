9e17c0945b6ef7140c195d2fc484c50eae0b6acc
andrewfurey21 <andrewfurey2003@gmail.com>
2025-07-24 18:15:54 +0100

    box class

---
+++ src/box.hpp
@@ -0,0 +1,91 @@
+#ifndef BOX_HPP
+#define BOX_HPP
+
+#include <vector>
+#include "shader.hpp"
+#include "camera.hpp"
+
+class Box {
+public:
+  Box(Shader shader) : shader(shader) {
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    glBindVertexArray(0);
+
+    model = glm::mat4(1.0f);
+  }
+
+  void draw(Camera& camera) {
+    shader.bind();
+    shader.setMat4("view", camera.view());
+    shader.setMat4("model", model);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+  }
+
+private:
+  unsigned int vao;
+  unsigned int vbo;
+
+  glm::mat4 model;
+
+  Shader shader;
+  std::vector<float> vertices = {
+        // positions          // normals           // texture coords
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
+  };
+
+};
+
+#endif
--- src/field.cpp
+++ src/field.cpp
@@ -1,19 +1,34 @@
+#include <glm/ext/vector_float3.hpp>
 #include <stdexcept>
 
 #include "field.hpp"
 #include "camera.hpp"
 #include "input.hpp"
+#include "shader.hpp"
+#include "box.hpp"
 
 int main(void) {
   int width = 800;
   int height = 600;
+  float near = 0.1f;
+  float far = 100.0f;
   GLFWwindow* window = initialize_glfw(width, height);
   initialize_glad();
   setup_window(window, width, height);
 
   Camera camera(45.0f, (float)width, (float)height,
-                  0.1f, 10.0f, 0.05f, 0.15f);
+                  near, far, 0.05f, 0.15f);
   Input input(window);
+
+  // ---------------------- Box -----------------------
+  Shader box_shader(
+    "/home/andrew/dev/graphics/renderer/shaders/basic_box_vertex.glsl",
+    "/home/andrew/dev/graphics/renderer/shaders/basic_box_fragment.glsl"
+  );
+  box_shader.setVec3("color", glm::vec3(1.0f, 0.0f, 0.0f));
+  box_shader.setMat4("projection", camera.projection());
+  Box box(box_shader);
+  // ----------------------------------------------------
 
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
@@ -24,6 +39,11 @@ int main(void) {
     input.mouse(camera, width, height);
     // ----------------------------------------------------
 
+    // ---------------------- Scene -----------------------
+
+    box.draw(camera);
+
+    // ----------------------------------------------------
     glfwGetFramebufferSize(window, &width, &height);
     glfwSwapBuffers(window);
     glfwPollEvents();
